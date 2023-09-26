125bc29c4e3debbd05b44262cce77b41bcd28595
Santosh Dahale <5417690+santoshdahale@users.noreply.github.com>
2023-09-26 17:57:53 +0530

    Update UnitUpgrader.java

--- module4/SOLID-main/liskov_substitution/bad/src/UnitUpgrader.java
+++ module4/SOLID-main/liskov_substitution/bad/src/UnitUpgrader.java
@@ -2,10 +2,11 @@
  * Created by mrk on 4/8/14.
  */
 public class UnitUpgrader {
-    public void upgrade(Apartment apartment) {
-        apartment.squareFootage += 40;
+    public StudioModified() {
+        this.numberOfRooms = 0;
+    }
 
-        if (apartment.getClass() != Studio.class)
-            apartment.numberOfBedrooms += 1;
+    public void setSquareFootage(int sqft) {
+        this.squareFootage = sqft;
     }
 }
